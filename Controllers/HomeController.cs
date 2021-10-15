a1004b47432b8980286b0012c8aa3d38eb21d852
Lolcoman <michaelkozak.cz@gmail.com>
2021-10-15 21:15:19 +0200

    update tabulky

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,11 +7,17 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace MVCProject.Controllers
 {
     public class HomeController : Controller
     {
+        SqlCommand command = new SqlCommand();
+        SqlDataReader dr;
+        List<Data> datas = new List<Data>();  
+        SqlConnection sqlConnection = new SqlConnection("Server=tcp:sqlusersdb.database.windows.net,1433;Initial Catalog=UsersDB;Persist Security Info=False;User ID=kozami01;Password=sql123?!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -48,6 +54,43 @@ namespace MVCProject.Controllers
         public IActionResult MainPage()
         {
             return View();
+        }
+        //získání dat z databáze
+        private void FillData()
+        {
+            if (datas.Count > 0)
+            {
+                datas.Clear();
+            }
+            try
+            {
+                sqlConnection.Open();
+                command.Connection = sqlConnection;
+                command.CommandText = "SELECT TOP (1000) [UserName],[Email],[Maze],[Pexeso],[Quiz],[DateTime] FROM [dbo].[UserTable]";
+                dr = command.ExecuteReader();
+                while (dr.Read())
+                {
+                    datas.Add(new Data() { UserName = dr["UserName"].ToString()
+                    ,Email = dr["Email"].ToString()
+                    ,Maze = dr["Maze"].ToString()
+                    ,Pexeso = dr["Pexeso"].ToString()
+                    ,Quiz = dr["Quiz"].ToString()
+                    ,DateTime = dr["DateTime"].ToString()
+                    
+                    });
+                }
+                sqlConnection.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+        public IActionResult Table()
+        {
+            FillData();
+            return View(datas);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Models/Data.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCProject.Models
+{
+    public class Data
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Maze { get; set; }
+        public string Pexeso { get; set; }
+        public string Quiz { get; set; }
+        public string DateTime { get; set; }
+
+    }
+}
